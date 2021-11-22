3a7c865afc0f18d2f47d7c6e390a4f0c757ab231
scrawleyy1 <crawley.sydney@gmail.com>
2021-11-22 12:08:58 -0800

    phasetwo

--- Program.cs
+++ Program.cs
@@ -4,9 +4,50 @@ namespace turducken
 {
     class Program
     {
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Plan your heist");
+            Console.WriteLine("---------------");
+
+            int MemberNum = 0;
+
+            string MemberList = null;
+            while (MemberList != "")
+            {
+                try
+                {
+                    Console.WriteLine("Enter Team Member Name");
+                    string TeamMemberName = Console.ReadLine();
+
+                    Console.WriteLine("Enter Team Member Skill Level");
+                    int TeamMemberSkill = Int32.Parse(Console.ReadLine());
+
+                    Console.WriteLine("Enter Team Member Courage Factor");
+                    double TeamMemberCourage = Convert.ToDouble(Console.ReadLine());
+
+                    MemberNum++;
+                    Console.WriteLine($"Member Count; {MemberNum}");
+
+                    TeamMember name = new TeamMember
+                    {
+                        Name = TeamMemberName,
+                        SkillLevel = TeamMemberSkill,
+                        CourageFactor = TeamMemberCourage
+                    };
+
+                    Console.WriteLine($"Name: {name.Name} Skill Level: {name.SkillLevel} Courage Factor: {name.CourageFactor}");
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("No input provided");
+                    return;
+                }
+
+            }
+
         }
+
     }
+
 }
