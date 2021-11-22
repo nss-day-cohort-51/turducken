51aa4de7c9919ea23b1c434e4b191f5938bcc2a1
joshuastewart417 <joshua.stewart780@gmail.com>
2021-11-22 13:21:08 -0600

    created TeamMember class and functionality to allow user to input Team Member info

--- Program.cs
+++ Program.cs
@@ -4,9 +4,30 @@ namespace turducken
 {
     class Program
     {
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Plan your heist");
+            Console.WriteLine("---------------");
+
+            Console.WriteLine("Enter Team Member Name");
+            string TeamMemberName = Console.ReadLine();
+
+            Console.WriteLine("Enter Team Member Skill Level");
+            int TeamMemberSkill = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter Team Member Courage Factor");
+            double TeamMemberCourage = Convert.ToDouble(Console.ReadLine());
+
+
+            TeamMember name = new TeamMember{
+                Name = TeamMemberName,
+                SkillLevel = TeamMemberSkill,
+                CourageFactor = TeamMemberCourage
+            };
+
+            Console.WriteLine($"Name: {name.Name} Skill Level: {name.SkillLevel} Courage Factor: {name.CourageFactor}");
         }
+
     }
 }
